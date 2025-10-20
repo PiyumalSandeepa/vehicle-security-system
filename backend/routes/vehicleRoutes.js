ed1d08427ed58c201146adc33998c1d450fee552
PiyumalSandeepa <mkpiyumalsandeepa@gmail.com>
2025-10-20 12:54:57 +0530

    reverse

--- backend/routes/vehicleRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vehicleController = require('../controllers/vehicleController');
-
-router.get('/get-vehicles', vehicleController.getVehicles);
-
-module.exports = router;
